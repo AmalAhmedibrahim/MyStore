f82a87ac342d26c15ce501435b6ba815ec626ffd
Amal Ahmed <dev.amal.ahmed@gmail.com>
2022-09-27 13:48:55 +0200

    update server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const path = require("path");
 const app = express();
+
 app.use(express.static(__dirname + "/dist/MyStore"));
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/MyStore/index.html"));
+app.get("/*", (req, res) => {
+  res.sendFile(__dirname + "/dist/MyStore/index.html");
 });
+
 app.listen(process.env.PORT || 8080);
